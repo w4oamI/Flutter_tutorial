298acf033da9fb935eae13316c5e5ed0c37b1010
w4oamI <ppas1515@naver.com>
2021-04-28 21:32:47 +0900

    페이지 디자인 학습1차

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -77,6 +77,8 @@ class MyApp extends StatelessWidget {
 //
 //
 //Widget 종류
+//---------------------------------------------//
+/*
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -104,6 +106,62 @@ class Mychar extends StatelessWidget {
           //Widget 세로정렬
           mainAxisAlignment: MainAxisAlignment.center, //Axis:가로,세로축
           children: [Text('로그인'), Text('회원가입'), Text('아이디찾기')],
+        ),
+      ),
+    );
+  }
+}
+*/
+//---------------------------------------------//
+//
+//
+//
+//
+//Page Design
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '창경궁',
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
+        title: Text('창경궁'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '로그인',
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0 //글자간격
+                  ),
+            ),
+            SizedBox(
+              height: 10.0, //로그인과 회원가입사이를 벌어줌
+            ),
+            Text('회원가입',
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold))
+          ],
         ),
       ),
     );
